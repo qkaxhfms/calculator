ffef3830d717816c3a77462d25edacece98beb6c
MS\kwakm <kwakms821@daum.net>
2021-03-14 23:57:21 +0900

    [ CI ] 더하기 구현하기

--- calculator.js
+++ calculator.js
@@ -23,52 +23,79 @@ let displayScreen = () => {
 // 계산
 const calculate = () => {
     let calculateProcess = [];
-    console.log('계산');
+    console.log('사칙연산');
+    // console.log(calculateProcess);
 };
+
+
+let step1 = [];
+
+let numberList = [];
+
+
+let result = 0;
+
+
+
 
 // 버튼 클릭 시
 const btns = Array.from(document.querySelectorAll('.btn'));
 btns.map((btn) => {
     btn.addEventListener('click', function () {
         // 계산 초기값
-        let defaultValueAll = {
-            numberBefore: 0,
-            numberAfter: 0,
-        };
+        // let defaultValueAll = {
+
+        //     numberBefore: [],
+        //     numberAfter: []
+        // };
 
         // 계산
         if (this.dataset.attr === 'operator') {
             // console.log('연산자')
-            calculate();
             if (this.value === '+') {
                 console.log('더하기');
+                let combineNumber = [];
+                let combineStep1 = step1.join("");
+                combineNumber.push(combineStep1);
+                numberList.push(combineNumber);
+                numberList.join(",")
+                // numberList.join(",");
+                
+                console.log(combineNumber);
+                // console.log(numberList);
+
             }
-            // if(this.value ==='-'){
-            //     console.log('뺴기')
-            // }
-            // if(this.value ==='x'){
-            //     console.log('곱하기')
-            // }
-            // if(this.value ==='/'){
-            //     console.log('나누기')
-            // }
+            if(this.value ==='-'){
+                console.log('빼기')
+            }
+            if(this.value ==='x'){
+                console.log('곱하기')
+            }
+            if(this.value ==='/'){
+                console.log('나누기')
+            }
+            if(this.value ==='cal'){
+                console.log('초기화')
+            }
         }
+
 
         // 숫자
         if (this.dataset.attr === 'number') {
             let numberCurrent = this.value;
-            console.log(numberCurrent);
-            // console.log(typeof this.value);
-            let numberBefores = Object.entries(defaultValueAll.numberBefore);
-            let numberBeforePush = numberBefores.push(numberCurrent);
-            console.log(numberBefores);
-            console.log(numberBeforePush);
-            // defaultValue.displayValue = this.value;
-            // console.log(defaultValue)
+            step1.push(numberCurrent);
         }
+
+
+
+        
+        // console.log(result);
+        // console.log(step1);
 
         // 상태를 보여줌
         // defaultValue.displayValue = this.value;
         // displayScreen();
     });
 });
+
+// console.log(result);
